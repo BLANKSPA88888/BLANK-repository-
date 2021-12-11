83df4be741a3aacdfdff0bede5429e7609c1a362
BLANKSPA88888 <89825067+BLANKSPA88888@users.noreply.github.com>
2021-12-11 14:25:22 +0800

    Add files via upload
    First upload.

---
+++ todolist/javascript.js
@@ -0,0 +1,18 @@
+let todolist = [
+    {title : "write a paper1";
+     isImportant:false;
+     isFinished:false;
+    {title : "write a paper2";
+     isImportant:false;
+     isFinished:false;
+}
+
+]
+
+
+
+
+function  renderTodoItemList(todoitems) {
+    let paneEl = document.querySelector('#todolist')
+    
+}
